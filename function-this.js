d0e1d9c42fecef7dcd09063c2124f911743fedf9
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2018-09-12 11:01:15 -0500

    Antes, despues, sin y con arrow function y this

--- function-this.js
+++ function-this.js
@@ -1,3 +1,15 @@
+/*var foot = {
+  kick: function () {
+      this.yelp = "Ouch!";
+      setImmediate(function () {
+          console.log(this.yelp);
+      });
+  }
+};
+foot.kick();*/
+
+
+
 var foot = {
   kick: function () {
       this.yelp = "Ouch!";
